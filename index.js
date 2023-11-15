db9b0ce1c83fe1106e6c2b47ffd54d3a9a53398f
pective <139483386+pective@users.noreply.github.com>
2023-11-15 15:09:09 +0100

    Started adding basic game mechanics

--- index.js
+++ index.js
@@ -4,7 +4,43 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
   const compChoice = options[Math.floor(Math.random() * options.length)];
-  console.log("The computer chose "+compChoice);
+  console.log(getComputerChoice);
+  return compChoice;
 }
+
+function Winner(playerChoice, computerChoice){
+  switch (playerChoice, computerChoice) {
+    case playerChoice == computerChoice:
+      console.log("Tie!");
+      break;
+    
+    case playerChoice == "rock" && computerChoice == "scissors":
+    case playerChoice == "paper" && computerChoice == "rock":
+    case playerChoice == "scissors" && computerChoice == "paper":
+      return "Player";
+      break;
+    
+     default:
+      return "Computer";
+      break;
+  }
+}
+
+function playGame(playerChoice, computerChoice) {
+  const result = Winner(playerChoice, computerChoice);
+  if(result == "Tie!"){
+    return"Its a tie!";
+  }
+  else if(result == "Player"){
+    return`You Win! ${playerChoice} beats {computerChoice}`;
+  }
+  else{
+    return`You lost! ${computerChoice} beats ${playerChoice}`
+  }
+}
+
+const playerChoice = "rock";
+const computerChoice = getComputerChoice();
+console.log(playGame(playerChoice, computerChoice));
 
 getComputerChoice()
