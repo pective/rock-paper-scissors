c980ba580fa9bfc30252fa4eeca0432ce9e18267
pective <139483386+pective@users.noreply.github.com>
2023-11-15 14:44:09 +0100

    Fixed bug not choosing a random option, instead printing out all options at once.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log("Log from the js file!");
 
-const options = ["rock, paper, scissors"];
+const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
   const compChoice = options[Math.floor(Math.random() * options.length)];
+  console.log("The computer chose "+compChoice);
 }
+
+getComputerChoice()
