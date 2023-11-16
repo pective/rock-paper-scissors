fb7cd8b54230d7968733032549919260d0a371d1
pective <139483386+pective@users.noreply.github.com>
2023-11-16 16:21:03 +0100

    Added the game function

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ console.log("Log from the js file!");
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-  const compChoice = options[Math.floor(Math.random() * options.length)];
-  console.log(compChoice);
+  const compChoice = Math.floor(Math.random() * options.length)
   return compChoice;
 }
 
@@ -38,6 +37,12 @@ function playGame(playerChoice, computerChoice) {
   }
 }
 
-const playerChoice = "rock";
-const computerChoice = getComputerChoice();
-console.log(playGame(playerChoice, computerChoice));
+function game(){
+  console.log("Welcome to pective's Rock Paper Scissors!");
+  for (let i = 0, i < 5, i++) {
+    const playerChoice = "rock" 
+    const computerChoice = getComputerChoice;
+    console.log(playGame(playerChoice, computerChoice)); 
+    }
+  }
+}
