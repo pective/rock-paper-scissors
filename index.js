699c70074aeeea1d688519ba28e7823025cb4196
pective <139483386+pective@users.noreply.github.com>
2023-11-16 16:46:22 +0100

    Rewrote the game function a bit

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ function playGame(playerChoice, computerChoice) {
   }
 }
 
-function game(){
-  console.log("Welcome to pective's Rock Paper Scissors!");
-  for (let i = 0, i < 5, i++) {
-    const playerChoice = "rock" 
-    const computerChoice = getComputerChoice;
-    console.log(playGame(playerChoice, computerChoice)); 
-    }
+function game() {
+  console.log("Welcome to pective's Rock Paper Scissors!")
+  for (let i = 0; i < 5; i++) {
+    const playerChoice = "rock";
+    const computerChoice = getComputerChoice();
+    console.log(playGame(playerChoice, computerChoice));
   }
 }
+
